0d6d3cb7846a7e69dbbc252f5b518ad8405b0694
Luiz Gustavo Setten <consultor.119@7comm.com.br>
2021-11-21 12:11:51 -0300

    feat: started entity

---
+++ src/controllers/recordController.ts
@@ -0,0 +1,52 @@
+import { Record } from "../entities/Record";
+import { Request, Response } from "express";
+import { createConnection } from "typeorm";
+
+export class RecordController {
+  public async create(request: Request, response: Response): Promise<void> {
+    try {
+
+      const record = new Record();
+
+      const { body: {
+        temperature,
+        humidity,
+        pressure,
+        solar_incidence,
+        wind_direction,
+        wind_speed,
+        station_id,
+      } } = request;
+
+      Object.assign(record, {
+        temperature,
+        humidity,
+        pressure,
+        solar_incidence,
+        wind_direction,
+        wind_speed,
+        station_id,
+      });
+
+      const conn = await createConnection();
+
+      const saved = await conn.manager.save(record);
+
+      response.send(saved);
+    } catch {
+      response.status(500).send();
+    }
+  }
+
+  list() {
+
+  }
+
+  findById() {
+
+  }
+
+  findLast() {
+
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import { RecordController } from './controllers/recordController';
 import express, { Request, Response } from 'express';
 import "reflect-metadata";
 import './config/datasabe';
@@ -5,14 +6,12 @@ import './config/datasabe';
 const app = express();
 app.use(express.json());
 
+const recordController = new RecordController()
+
 app.get('/', (request: Request, response: Response) => {
   response.send(200);
 });
 
-app.post('/', (request: Request, response: Response) => {
-  console.log(request.body);
-
-  response.send(200);
-});
+app.post('/', recordController.create);
 
 app.listen(3333, () => console.log('Server is listening on http://localhost:3333/'));
