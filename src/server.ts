4cf4c20ca39eba1af2343453669b35753b82d6fc
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2022-06-13 01:20:12 -0300

    fix: repo os aws

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import express, { Request, Response } from 'express';
+import express, { Request, Response, Router } from 'express';
 import cors from 'cors';
 
 import './config/datasabe';
@@ -11,37 +11,39 @@ import { UsersController } from './controllers/usersController';
 import { StationController } from './controllers/stationController';
 
 const app = express();
+const router = Router();
 app.use(express.json());
 app.use(cors());
+app.use('/api', router);
 
 const recordController = new RecordController();
 const logsController = new LogsController();
 const usersController = new UsersController();
 const stationController = new StationController();
 
-app.get('/', (request: Request, response: Response) => {
+router.get('/', (request: Request, response: Response) => {
   console.log('Refletiu aqui');
   response.sendStatus(200);
 });
 
-app.post('/users', usersController.create.bind(usersController));
-app.post('/users/authenticate', usersController.authenticate.bind(usersController));
-app.put('/users', ensureAuthenticated, usersController.update.bind(usersController));
-app.get('/users', ensureAuthenticated, usersController.list);
+router.post('/users', usersController.create.bind(usersController));
+router.post('/users/authenticate', usersController.authenticate.bind(usersController));
+router.put('/users', ensureAuthenticated, usersController.update.bind(usersController));
+router.get('/users', ensureAuthenticated, usersController.list);
 
-app.get('/stations', stationController.list);
-app.get('/stations/mine', ensureAuthenticated, stationController.listByUser.bind(stationController));
-app.post('/stations', ensureAuthenticated, stationController.create.bind(stationController));
-app.put('/stations', ensureAuthenticated, stationController.update.bind(stationController));
-// app.delete('/stations', ensureAuthenticated, stationController.update);
+router.get('/stations', stationController.list);
+router.get('/stations/mine', ensureAuthenticated, stationController.listByUser.bind(stationController));
+router.post('/stations', ensureAuthenticated, stationController.create.bind(stationController));
+router.put('/stations', ensureAuthenticated, stationController.update.bind(stationController));
+// router.delete('/stations', ensureAuthenticated, stationController.update);
 
-app.get('/records', recordController.list);
-app.get('/records/:station_id', recordController.list);
-app.get('/records/last/:station_id', recordController.findLast);
-app.post('/records', recordController.create);
+router.get('/records', recordController.list);
+router.get('/records/:station_id', recordController.list);
+router.get('/records/last/:station_id', recordController.findLast);
+router.post('/records', recordController.create);
 
-app.get('/updateLogs', logsController.update);
-app.get('/logs/:station_id/:reference_date_min/:reference_date_max', logsController.list);
-app.get('/logs/:station_id/:reference_date_min/:reference_date_max/download', logsController.downloadCSV);
+router.get('/updateLogs', logsController.update);
+router.get('/logs/:station_id/:reference_date_min/:reference_date_max', logsController.list);
+router.get('/logs/:station_id/:reference_date_min/:reference_date_max/download', logsController.downloadCSV);
 
 app.listen(3333, () => console.log('Server is listening on http://localhost:3333/'));
